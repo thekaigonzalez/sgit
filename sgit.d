e48867098654c9bfe7448ed7b93472464f5812dc
Kai D. Gonzalez <mail>
2021-09-11 12:38:25 -0400

    sgit help

--- sgit.d
+++ sgit.d
@@ -5,8 +5,10 @@ void main(string[] args) {
  bool initialize_repo = false;
  string initial_branch_name = "none";
  string iremote = "none";
+ bool help_need =false;
  try {
-  getopt(args, std.getopt.config.bundling, "init|i", &initialize_repo, "branch-name|b", &initial_branch_name, "remote|r", &iremote);
+  getopt(args, std.getopt.config.bundling, "init|i", &initialize_repo, "branch-name|b", &initial_branch_name, "remote|r", &iremote,
+  "help|h", &help_need);
  } catch (GetOptException e) { writeln("bad"); }
  if (initialize_repo == true) {
   executeShell("git init");
@@ -17,5 +19,9 @@ void main(string[] args) {
    executeShell("git remote add origin "~iremote);
    writeln("done!");
   }
+ }
+ if (help_need) {
+  writeln("usage: sgit -[ibr]");
+  writeln("SGit is a git minifier.\nThis is used by KS for quickly building git repositories.");
  }
 }
